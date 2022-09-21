27d70689bc92a25017fb81bf14e4a489708b249c
Gabriel Sappio <100438648+GSappio@users.noreply.github.com>
2022-09-21 11:02:09 -0300

    Using classes and MVC to add form information to layout

--- index.js
+++ index.js
@@ -13,25 +13,22 @@ var user = {};
 
 function addLine(dataUser) {
 
-    console.log("addline", dataUser);
+    console.log(dataUser);
 
-    var tr = document.createElement("tr");
-
-    tr.innerHTML = `
+    document.getElementById("table-users").innerHTML = `
         <tr>
             <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
             <td>${dataUser.name}</td>
             <td>${dataUser.email}</td>
             <td>${dataUser.admin}</td>
             <td>${dataUser.birth}</td>
-            <td>
+            <td>    
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
         </tr>
     `;
 
-    document.getElementById("table-users").appendChild(tr);
 
 }
 
@@ -56,6 +53,17 @@ document.getElementById("form-user-create").addEventListener("submit", function(
 
     });
 
-    addLine(user);
+    var objectUser = new User(
+        user.name,
+        user.gender,
+        user.birth,
+        user.country,
+        user.email,
+        user.password,
+        user.photo,
+        user.admin
+    );
+
+    addLine(objectUser);
 
 });
